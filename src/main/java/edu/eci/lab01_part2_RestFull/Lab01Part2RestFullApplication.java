d619e835198d18536dedcb73241a7c12434bf728
Johann Bogota <johann.bogota@escuelaing.edu.co>
2021-08-24 17:59:50 -0500

    First files Rest

---
+++ src/main/java/edu/eci/lab01_part2_RestFull/Lab01Part2RestFullApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.lab01_part2_RestFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01Part2RestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab01Part2RestFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/lab01_part2_RestFull/Lab01Part2RestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.lab01_part2_RestFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01Part2RestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
